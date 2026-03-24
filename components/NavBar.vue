51933980e0295a19d5cdd5af5b13e8f902839283
msajath <mohamedsajath10867@gmail.com>
2026-03-24 22:17:55 +0530

    Setup the json package

---
+++ components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="bg-white shadow">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div class="flex justify-between h-16">
+        <div class="flex items-center">
+          <h1 class="text-2xl font-bold text-gray-900">GUI Final Project</h1>
+        </div>
+        <div class="flex space-x-4 items-center">
+          <router-link to="/" class="text-gray-900 hover:text-gray-700">Home</router-link>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+</style>
---
+++ dist/assets/App-a04d239f.js
@@ -0,0 +1,1 @@
+import{_ as f}from"./index-b37c5b25.js";export{f as default};
