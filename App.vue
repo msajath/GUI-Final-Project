b0bc6a478c9b00cf11d4d0a43fadc7c66fff519d
msajath <mohamedsajath10867@gmail.com>
2026-03-24 22:08:38 +0530

    Configure main entry file and base app layout

---
+++ App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="min-h-screen">
+    <NavBar />
+    <main class="max-w-7xl mx-auto px-4 py-6">
+      <RouterView />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue'
+</script>
